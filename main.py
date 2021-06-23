dc0fcddb4fcfc6f8bef300b00b17808faa5c3d36
Kanghee Cho <52508094+DingleBells@users.noreply.github.com>
2021-06-23 22:54:58 +0000

    Finished my first discord encourage bot, might come back to it to test new things out. 23.6.2021

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import discord
 import os
+import requests
+import json
+from keep_alive import keep_alive
+
 
 client = discord.Client()
 
-@client.event
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
 
+
+@client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
@@ -12,6 +22,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-client.run(os.getenv('TOKEN'))
+
+    if message.content.startswith("$"):
+          if message.channel.name != "encourage-bot-testing":
+            await message.channel.send("Please only use this bot in <#857377863941357568>")
+          
+          elif message.content.startswith('$hello'):
+              await message.channel.send('Hello!')
+          
+          elif message.content.startswith('$inspire'):
+            await message.channel.send(get_quote())
+
+token = os.environ['TOKEN']
+
+keep_alive()
+client.run(os.environ['TOKEN'])
